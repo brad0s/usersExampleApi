171d8c1d8a9fffdeec8ca53f423bb91d8b124c58
Braden Wright <brad0swrizzy@gmail.com>
2021-10-05 10:17:13 -1000

    basic server setup

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-// TODO
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.listen(process.env.PORT, () =>
+  console.log(`server has started at port ${process.env.PORT}`)
+);
