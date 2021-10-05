ba82aef53a0197988ba7c0ebb566ea9b33863cdb
Braden Wright <brad0swrizzy@gmail.com>
2021-10-05 13:17:14 -1000

    Add CRUD functionality

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -3,34 +3,84 @@ import User from '../models/User.js';
 
 const router = express.Router();
 
+// getUser middleware
+const getUser = async (req, res, next) => {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (user === null) {
+      return res.status(404).json({ message: 'Cannot find User' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.user = user;
+  next(); // move to the next section of the code and execute it
+};
+
 // Get All Route
 router.get('/', async (req, res) => {
-  // Rest of the code will go here
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Get One Route
-router.get('/:id', async (req, res) => {
-  // Rest of the code will go here
+router.get('/:id', getUser, (req, res) => {
+  res.json(res.user);
 });
 
 // Create One Route
 router.post('/', async (req, res) => {
-  // Rest of the code will go here
+  const user = new User({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  });
+  try {
+    const newUser = await user.save();
+    res.status(201).json({ newUser });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 // Edit One Route PUT version
-router.put('/:id', async (req, res) => {
-  // Rest of the code will go here
+router.put('/:id', getUser, async (req, res) => {
+  try {
+    const updatedUser = await res.user.set(req.body);
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // Edit One Route PATCH version
-router.patch('/:id', async (req, res) => {
-  // Rest of the code will go here
+router.patch('/:id', getUser, async (req, res) => {
+  if (req.body.firstname != null) {
+    res.user.firstname = req.body.firstname;
+  }
+  if (req.body.lastname != null) {
+    res.user.lastname = req.body.lastname;
+  }
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 // Delete One Route
-router.delete('/:id', async (req, res) => {
-  // Rest of the code will go here
+router.delete('/:id', getUser, async (req, res) => {
+  try {
+    await res.user.deleteOne();
+    res.json({ message: 'User has been deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 export default router;
